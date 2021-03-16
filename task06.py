8415c7f9b9233ad6dffa4de0a153b3e90b97d9eb
Buhle <77104529+Buhle0000@users.noreply.github.com>
2021-03-16 15:55:53 +0200

    Delete task06.py

--- task06.py
+++
@@ -1,4 +0,0 @@
-number = [int(number) for number in input("Enter three different numbers(hit space after each number):"   " ").split()]
-max_number = max(number)
-print(f"maximum number is {max_number}")
-
