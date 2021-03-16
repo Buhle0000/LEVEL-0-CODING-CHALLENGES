dcfd3c1ce2f2f89e3d5e9442825294614182292f
Buhle <77104529+Buhle0000@users.noreply.github.com>
2021-03-16 15:55:40 +0200

    Delete task010.py

--- task010.py
+++
@@ -1,8 +0,0 @@
-word_1 = input(str("Enter first word: "))
-word_2 = input(str("Enter second word: "))
-characters = list(set(word_1)&set(word_2))
-
-print("The common characters is: ")
-for alphabets in characters:
-    print(alphabets)
-
