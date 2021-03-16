e5b6fc248306c1ea6ace3386be8762317a71798e
Buhle <77104529+Buhle0000@users.noreply.github.com>
2021-03-16 14:24:18 +0200

    Delete task07_f_to_c.py

--- task07_f_to_c.py
+++
@@ -1,8 +0,0 @@
-fahrenheit = float(input("What's the fahrenheit(f)? :"))
-def convert(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
-
-celsius = convert(fahrenheit)
-print(f"temperature in celsius is {celsius}")
-
