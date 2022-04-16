ed480ec505b52cd5dcc77d9ab03efeb6f1168c4f
Buhle <77104529+Buhle0000@users.noreply.github.com>
2022-04-16 19:38:43 +0200

    Update task05.py

--- task05.py
+++ task05.py
@@ -1,5 +1,5 @@
-def area_of_triangle(a, b, c):
-    semi_perimeter = (a + b +c) * 0.5
-    area = str((semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) * (semi_perimeter - c)) ** 0.5)
+def area_of_triangle(side1, side2, side3):
+    semi_perimeter = (side1 + side2 +side3) * 0.5
+    area = str((semi_perimeter * (semi_perimeter - side1) * (semi_perimeter - side2) * (semi_perimeter - side3)) ** 0.5)
     return area
     
